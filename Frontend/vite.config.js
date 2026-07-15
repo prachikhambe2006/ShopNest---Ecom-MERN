8639d8a6c3641b505d17f78f1d5a439693f938c0
Prachi <Prachi@gmail.com>
2026-07-15 14:53:18 +0530

    URL

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
  server:{
    proxy:{
      '/api':{
-       target:'http://localhost:5000',
+       target:'http://shopnest-c52n.onrender.com/',
        changeOrigin:true
      }
    }
