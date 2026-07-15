362c3e033837d0d8af66c557748ee59812439088
Prachi <Prachi@gmail.com>
2026-07-15 15:59:00 +0530

    https

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
  server:{
    proxy:{
      '/api':{
-       target: 'http://shopnest-c52n.onrender.com' || 'http://localhost:5000',
+       target: 'https://shopnest-c52n.onrender.com' || 'https://localhost:5000',
        changeOrigin:true
      }
    }
